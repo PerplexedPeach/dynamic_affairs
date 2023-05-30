bcd8210a35a59d12db67a3cc95c0792a5c2ce215
LazyIcarus <kleinmorett@gmail.com>
2023-05-30 01:06:04 -0400

    Generalize compatibility generation to scoped names

--- create_compatible_files.py
+++ create_compatible_files.py
@@ -21,14 +21,18 @@ with open(settings_file, 'wb') as f:
     pickle.dump(settings, f)
 
 
-def create_combined_compatible_file(relative_path, num_trailing_braces, game_directory=None):
+def create_combined_compatible_file(relative_path, enclosing_scope_name, game_directory=None):
     """
     Create a merged copy of a file that is compatible with the game.
     Look for our source under relative_path, find the same game file under the game_directory and merge them by
     adding our file content at the end, preceding a specified number of closing braces.
 
     :param relative_path: relative path to the file we want to merge
-    :param num_trailing_braces: number of closing braces our content precedes
+    :param enclosing_scope_name: name or fullname of the enclosing scope you want to add the file's content to. e.g.
+    "western_royalty" will search for the block western_royalty = { ... and add the file's content before the closing
+    brace. A fullname or partial scoped named like "cloaks:western_royalty" can also be used, which will search for
+    the block cloaks = { ... western_royalty = { ... and add the file's content before the closing brace of the
+    inside western_royalty block.
     :param game_directory: None to specify the game directory, otherwise a full path to for example another mod like CPF
     e.g. C:\Program Files (x86)\Steam\steamapps\workshop\content\1158310\2220098919
     :return:
@@ -55,16 +59,31 @@ def create_combined_compatible_file(relative_path, num_trailing_braces, game_dir
     with open(game_file, 'r', encoding='utf-8') as f:
         game_content = f.read().strip(UTF8_BOM)
 
+    # find the innermost enclosing scope
+    scopes = enclosing_scope_name.split(":")
+    idx = 0
+    for scope in scopes:
+        idx = game_content.find(scope, idx)
+        if idx == -1:
+            raise Exception("Couldn't find scope " + scope + " in " + game_file)
+    # we are now at the start of the scope name, go to the end of the scope by finding the matching brace
+    istart = []
+    i = 0
+    for i, c in enumerate(game_content[idx:]):
+        if c == '{':
+            istart.append(i)
+        elif c == '}':
+            istart.pop()
+            if not istart:
+                break
+    idx += i
+
     # insert src_content before the last num_trailing_braces closing braces
-    # find the last num_trailing_braces closing braces
-    last_brace_index = -1
-    for i in range(num_trailing_braces):
-        last_brace_index = game_content.rfind("}", 0, last_brace_index)
     # go back to the first non-whitespace character
-    while game_content[last_brace_index - 1] in [" ", "\t", "\n", "\r"]:
-        last_brace_index -= 1
+    while game_content[idx - 1] in [" ", "\t", "\n", "\r"]:
+        idx -= 1
     # insert src_content before the last num_trailing_braces closing braces
-    game_content = game_content[:last_brace_index] + '\n' + src_content + game_content[last_brace_index:]
+    game_content = game_content[:idx] + '\n' + src_content + game_content[idx:]
     # write the combined content to a copy of the game file at relative_path
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     print("Writing combined file to " + output_path)
@@ -81,10 +100,10 @@ if game_dir.strip() == "":
     game_dir = None
 
 # put any other files that need to be made compatible here
-create_combined_compatible_file("gfx/portraits/portrait_modifiers/00_custom_clothes.txt", 1, game_dir)
-create_combined_compatible_file("gfx/portraits/portrait_modifiers/00_custom_headgear.txt", 1, game_dir)
-create_combined_compatible_file("common/genes/05_genes_special_accessories_clothes.txt", 3, game_dir)
-create_combined_compatible_file("common/genes/06_genes_special_accessories_headgear.txt", 2, game_dir)
-create_combined_compatible_file("gfx/portraits/portrait_modifiers/CFP_necklaces.txt", 1, game_dir)
-create_combined_compatible_file("common/genes/CFP_genes_special_accessories_necklaces.txt", 3, game_dir)
-create_combined_compatible_file("gfx/portraits/portrait_modifiers/CFP_custom_necklaces.txt", 1, game_dir)
+create_combined_compatible_file("gfx/portraits/portrait_modifiers/00_custom_clothes.txt", "custom_clothes", game_dir)
+create_combined_compatible_file("gfx/portraits/portrait_modifiers/00_custom_headgear.txt", "custom_headgear", game_dir)
+create_combined_compatible_file("common/genes/05_genes_special_accessories_clothes.txt", "accessory_genes:clothes", game_dir)
+create_combined_compatible_file("common/genes/06_genes_special_accessories_headgear.txt", "accessory_genes:headgear", game_dir)
+create_combined_compatible_file("gfx/portraits/portrait_modifiers/CFP_necklaces.txt", "cfp_necklaces", game_dir)
+create_combined_compatible_file("common/genes/CFP_genes_special_accessories_necklaces.txt", "special_accessories_necklace", game_dir)
+create_combined_compatible_file("gfx/portraits/portrait_modifiers/CFP_custom_necklaces.txt", "custom_necklace", game_dir)
